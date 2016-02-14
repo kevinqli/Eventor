cf8c8bd7cfb5cc9ff2ab9fa64bb03c829a015f33
Kevin Q Li <likevinq@gmail.com>
2016-02-14 05:03:34 -0800

    Added dynamic group list on nav bar.

---
+++ app/src/main/java/com/eventor/eventor/GroupPageActivity.java
@@ -0,0 +1,13 @@
+package com.eventor.eventor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_page);
+    }
+}
