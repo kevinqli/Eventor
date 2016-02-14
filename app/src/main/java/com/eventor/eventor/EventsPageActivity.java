d4f0a864d5075345b4b0e5be98bf90f2169999e2
tungalbert99 <tung.albert99@gmail.com>
2016-02-14 04:37:25 -0800

    improved events page activity UI

--- app/src/main/java/com/eventor/eventor/EventsPageActivity.java
+++ app/src/main/java/com/eventor/eventor/EventsPageActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class EventsPageActivity extends AppCompatActivity {
 
@@ -16,15 +19,28 @@ public class EventsPageActivity extends AppCompatActivity {
 
         final TextView eventTitle = (TextView) findViewById(R.id.eventTitle);
         final TextView eventDate = (TextView) findViewById(R.id.date);
+        final TextView eventTime = (TextView) findViewById(R.id.time);
         final TextView eventLocation = (TextView) findViewById(R.id.location);
         final TextView eventDescription = (TextView) findViewById(R.id.description);
 
         Intent intent = getIntent();
         Event event = Event.getEvent(intent.getIntExtra("ITEMINDEX", -1));
 
+        SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("MM.dd.yyyy");
+
+        Date d = event.getEventStart();
+        String startTimeText = sdf.format(d);
+        String startDateText = sdf2.format(d);
+
+        Date d2 = event.getEventEnd();
+        String endTimeText = sdf.format(d2);
+        String endDateText = sdf2.format(d2);
+
 
         eventTitle.setText(event.getEventName());
-        eventDate.setText(event.getEventStart().getDate() + " to " + event.getEventEnd().getDate());
+        eventDate.setText(startDateText + " to " + endDateText);
+        eventDate.setText(startTimeText + " to " + endTimeText);
         eventLocation.setText("Stanford");
         eventDescription.setText(event.getEventDesc());
     }
