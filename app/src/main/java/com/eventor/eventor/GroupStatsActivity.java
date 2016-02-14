419a3f69756bd1b0ae7c0d15733ba500780b90e0
Kevin Q Li <likevinq@gmail.com>
2016-02-14 07:30:07 -0800

    Made graph nice

--- app/src/main/java/com/eventor/eventor/GroupStatsActivity.java
+++ app/src/main/java/com/eventor/eventor/GroupStatsActivity.java
@@ -21,8 +21,8 @@ public class GroupStatsActivity extends AppCompatActivity {
         BarChart chart = (BarChart) findViewById(R.id.chart);
 
         BarData data = new BarData(getXAxisValues(), getDataSet());
+        chart.setDescription("");
         chart.setData(data);
-        chart.setDescription("My Chart");
         chart.animateXY(2000, 2000);
         chart.invalidate();
     }
@@ -31,37 +31,37 @@ public class GroupStatsActivity extends AppCompatActivity {
         ArrayList<BarDataSet> dataSets = null;
 
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        BarEntry v1e1 = new BarEntry(5, 0); // Jan
         valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        BarEntry v1e2 = new BarEntry(7, 1); // Feb
         valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        BarEntry v1e3 = new BarEntry(10, 2); // Mar
         valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        BarEntry v1e4 = new BarEntry(8, 3); // Apr
         valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        BarEntry v1e5 = new BarEntry(15, 4); // May
         valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        BarEntry v1e6 = new BarEntry(24, 5); // Jun
         valueSet1.add(v1e6);
 
         ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+        BarEntry v2e1 = new BarEntry(6, 0); // Jan
         valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+        BarEntry v2e2 = new BarEntry(9, 1); // Feb
         valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+        BarEntry v2e3 = new BarEntry(12, 2); // Mar
         valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+        BarEntry v2e4 = new BarEntry(25, 3); // Apr
         valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+        BarEntry v2e5 = new BarEntry(42, 4); // May
         valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+        BarEntry v2e6 = new BarEntry(49, 5); // Jun
         valueSet2.add(v2e6);
 
         BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
         barDataSet1.setColor(Color.rgb(0, 155, 0));
         BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet2.setColor(Color.rgb(155, 0, 0));
 
         dataSets = new ArrayList<>();
         dataSets.add(barDataSet1);
@@ -71,12 +71,12 @@ public class GroupStatsActivity extends AppCompatActivity {
 
     private ArrayList<String> getXAxisValues() {
         ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
+        xAxis.add("8");
+        xAxis.add("10");
+        xAxis.add("12");
+        xAxis.add("14");
+        xAxis.add("16");
+        xAxis.add("18");
         return xAxis;
     }
 }
