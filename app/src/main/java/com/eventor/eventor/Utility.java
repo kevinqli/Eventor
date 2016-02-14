c5bfc88177065f8d9e56f44f93f22a66dc4a8ca6
Jeffrey Zhang <jeffreyzhang@DN525en8.SUNet>
2016-02-14 03:47:04 -0800

    yo3

--- app/src/main/java/com/eventor/eventor/Utility.java
+++ app/src/main/java/com/eventor/eventor/Utility.java
@@ -10,6 +10,7 @@ public class Utility {
 
         for(Integer id : users) {
             User curUser = User.users_data.get(id);
+            int tmp[] = new int[ret.length];
             for(Integer id2 : curUser.getEventList()) {
                 Event curEvent = Event.events_data.get(id2);
                 Date begin = curEvent.getEventStart();
@@ -17,17 +18,21 @@ public class Utility {
 
                 if(end.getDay() < query.getDay()) continue;
                 if(begin.getDay() > query.getDay()) continue;
-                
+
                 int idx1 = (begin.getHours() * 60 + begin.getMinutes()) / 30;
                 if(begin.getDay() < query.getDay())
                     idx1 = 0;
                 int idx2 = (end.getHours() * 60 + end.getMinutes()) / 30;
                 if(end.getDay() > query.getDay())
                     idx2 = ret.length - 1;
-                ret[idx1]++;
+                tmp[idx1]++;
                 if(idx2 + 1 < ret.length) {
-                    ret[idx2 + 1]--;
+                    tmp[idx2 + 1]--;
                 }
+            }
+            for(int i = 0; i < ret.length; i++) {
+                if(tmp[i] > 0) ret[i] = 1;
+                if(tmp[i] < 0) ret[i] = -1;
             }
         }
         for(int i = 0; i < ret.length; i++) {
