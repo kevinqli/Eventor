ed024128db132b6416e26ff82baef2732d084425
Eric Xu <ericxu0@stanford.edu>
2016-02-13 16:04:37 -0800

    friendsPage + userPage

---
+++ app/src/main/java/com/vibe/vibe/userPage.java
@@ -0,0 +1,58 @@
+package com.vibe.vibe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.*;
+
+public class userPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+
+        Spinner following = (Spinner) findViewById(R.id.following);
+        String[] followingItems = new String[] { "Following", "Stanford TreeHacks", "Stanford CS", "Stanford Football" };
+        ArrayAdapter<String> followingAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, followingItems);
+        following.setAdapter(followingAdapter);
+        following.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Spinner events = (Spinner) findViewById(R.id.events);
+        String[] eventsItems = new String[] { "Upcoming Events", "Stanford TreeHacks", "CS Career Fair", "Stanford vs. Cal" };
+        ArrayAdapter<String> eventsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, eventsItems);
+        following.setAdapter(eventsAdapter);
+        following.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button firstButton = (Button) findViewById(R.id.friendsButton);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(userPage.this, friends_page.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
